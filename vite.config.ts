7c7e3eee9c14b9d7ea6dec69ff93de7fe006e354
Nico-Alarcon <na2946@columbia.edu>
2025-01-29 14:08:53 -0500

    reset vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Nico-Alarcon.github.io/', // Replace 'portfolio' with your repository name
+  base: './', // Replace 'portfolio' with your repository name
   server: {
     port: 5173,
     host: true
