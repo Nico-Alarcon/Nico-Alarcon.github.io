ee4bb7846cc5257b0cb01be78f1c45a34d920015
Nico-Alarcon <na2946@columbia.edu>
2025-03-03 12:53:31 -0500

    update CSP, use hash router

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Replace 'portfolio' with your repository name
+  base: '/', // Changed from '/Nico-Alarcon.github.io/' to '/'
   server: {
     port: 5173,
     host: true
